665eaf1449e802e7aef2ae8168d450c4130963bc
Artem Zhyto <artemzhr11@gmail.com>
2026-07-17 23:17:35 +0300

    fix: update icon configurations in manifest.ts

--- front/src/app/manifest.ts
+++ front/src/app/manifest.ts
@@ -23,16 +23,30 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#171219',
     icons: [
       {
-        src: './icon-192.png',
+        src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: './icon-512.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
+      },
+    ],
+    shortcuts: [
+      {
+        name: siteConfig.title.default,
+        url: '/',
+        icons: [
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
       },
     ],
   }
