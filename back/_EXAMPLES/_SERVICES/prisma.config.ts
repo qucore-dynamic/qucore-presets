c83bd977d19ad68da7be84684ca01de76a8c786e
Artem Zhyto <artemzhr11@gmail.com>
2026-07-18 16:26:57 +0300

    fix: resolve Prisma configuration and export PrismaClient instance

--- back/_EXAMPLES/_SERVICES/prisma.config.ts
+++ back/_EXAMPLES/_SERVICES/prisma.config.ts
@@ -1,5 +1,9 @@
 // Modules
+import path from 'path'
+import dotenv from 'dotenv'
 import { defineConfig } from 'prisma/config'
+
+dotenv.config({ path: path.resolve(process.cwd(), '../../.env') })
 
 export default defineConfig({
   schema: `prisma/schema.prisma`,
--- back/_EXAMPLES/_SERVICES/src/config.ts
+++ back/_EXAMPLES/_SERVICES/src/config.ts
@@ -1,10 +1,22 @@
+// Configs
+import { PrismaClient } from '../prisma/generated/client'
+
 // Modules
+import { PrismaPg } from '@prisma/adapter-pg'
 import path from 'path'
 import dotenv from 'dotenv'
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env') })
+dotenv.config({ path: path.resolve(process.cwd(), '../../.env') })
 
 const isProd = process.env.MODE === 'prod'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const PRISMA = new PrismaClient({
+  adapter: adapter,
+})
 
 export const __PORT = process.env.<SERVICE_NAME>
 export const cookieOptions = {
@@ -14,3 +26,5 @@ export const cookieOptions = {
   ...(isProd && { domain: process.env.DOMAIN || '.qucore.io' }),
   signed: true,
 }
+
+export default PRISMA
